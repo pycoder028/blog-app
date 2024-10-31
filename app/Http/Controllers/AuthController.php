693a3ab97a5341e4c957ca1ebbc66a4d1bfc3c77
Riyad <pycoder028@gmail.com>
2024-10-31 22:47:16 +0600

    Login Register Forgot html page setup and DB connect

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    
+    public function login(){
+        return view('auth.login');
+    }
+
+    public function register(){
+        return view('auth.register');
+    }
+
+    public function forgotPassword(){
+        return view('auth.forgot');
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login',[AuthController::class, 'login']);
+Route::get('register',[AuthController::class, 'register']);
+Route::get('forgot-password',[AuthController::class, 'forgotPassword']);
