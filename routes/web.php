ed039fe5c1738c3131e7b56d20f0d6cbe5bd598e
Riyad <pycoder028@gmail.com>
2024-11-01 13:49:27 +0600

    Blog template setup

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[HomeController::class,'index']);
 
 Route::get('login',[AuthController::class, 'login']);
 Route::get('register',[AuthController::class, 'register']);
